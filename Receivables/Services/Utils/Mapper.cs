e11c4c83f50925992289585862b9a7917ce18e7e
nick-souza <nsds26@hotmail.com>
2024-11-08 19:54:10 -0300

    feat: mappers

---
+++ Receivables/Services/Utils/Mapper.cs
@@ -0,0 +1,24 @@
+﻿using Receivables.DTO.Outgoing;
+using Receivables.Entities;
+
+namespace Receivables.Services.Utils;
+
+public abstract class Mapper
+{
+    public static InvoiceDto MapToInvoiceDto(Invoice invoice) => new()
+    {
+        number = invoice.Number,
+        cnpj = invoice.Cnpj,
+        value = invoice.Value,
+        dueDate = invoice.DueDate.ToString("dd/MM/yyyy")
+    };
+
+    public static CompanyDto MapToCompanyDto(Company company) => new()
+    {
+        cnpj = company.Cnpj,
+        name = company.Name,
+        sector = company.Sector.ToString(),
+        monthlyBilling = company.MonthlyBilling,
+        invoices = company.Invoices.Select(MapToInvoiceDto).ToList()
+    };
+}
