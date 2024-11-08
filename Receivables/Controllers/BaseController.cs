e4e5a0413ca100809fb2f2681d85718e5e0ab4bd
nick-souza <nsds26@hotmail.com>
2024-11-08 19:54:03 -0300

    feat: base controller

---
+++ Receivables/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Receivables.DTO.Outgoing;
+
+namespace Receivables.Controllers;
+
+public class BaseController : ControllerBase
+{
+    protected ObjectResult OkResponse() => StatusCode(200, new Response(true));
+    protected ObjectResult OkResponse<T>(T data) => StatusCode(200, new Response<T>(true, data));
+}
